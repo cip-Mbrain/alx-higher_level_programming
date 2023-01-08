f37a24fd772e3590776c50885de2f1072b3a0932
Machine_brain <ennocentcip@gmail.com>
2023-01-08 09:29:39 -0800

    a commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     arr = []
     for i in my_string:
-        if i == "c" ot i == "C":
+        if i == "c" or i == "C":
             arr.append('')
         else:
             arr.append(1)
