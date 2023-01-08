15d5603534acf2b4c126828c547855853f7f7433
Machine_brain <ennocentcip@gmail.com>
2023-01-07 17:57:28 -0800

    a commit

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,24 +1,2 @@
-#!/bin/usr/python3
-
-print("The Zen of Python, by Tim Peters\n")
-
-print("Beautiful is better than ugly.")
-print("Explicit is better than implict.")
-print("Complex is better than complicated.")
-print("Flat is netter than nested.")
-print("Spare is better than dense.")
-print("Readability counts.")
-print("Special case aren't special enough to breal the rules.")
-print("Although practicality beats purity.")
-print("Erros should never pass silently.")
-print("Unless explicitly silently.")
-print("In the face of ambiguity, refuse the temptation to guess")
-print("There should be one and preferalbe only one --obvious way to do it.")
-print("Although that way may not be obvious at first unless you're Dutch.")
-print("Now is better than never")
-print("Although never is often better than *right now.")
-print("If the implementation is hard to explain, it's a bad idea")
-print("If the implementation is  easy to explain, it may be a good idea")
-print("Namespace are one honking great idea --let's do more of those!")
-
-
+#!/bin/usr/pythn3
+import this
